5dee3131bc948df02f081af8d54fe9908b63e774
Akshat Jain <akshatjpr03@gmail.com>
2025-02-28 00:03:56 +0530

    Completed Async Js and Started Machine Coding Concept Proxy

--- 03_JavaScript/03_Basics/02_scopes.js
+++ 03_JavaScript/03_Basics/02_scopes.js
@@ -1,4 +1,5 @@
-// {}  // These curly braces indicate scope of function or if else condition .. obejct curly braces are different.. vaise feel agyi hai 
+// {}  // These curly braces indicate scope of function or if else condition .. obejct curly braces are different..
+//  vaise feel agyi hai 
 // var ke sath dikkat ye hi thi ki vo scope ke hisaab se nhi chalta tha , iss vajah se sab let ko use karte hai
 var c=300
 if(true){
@@ -9,7 +10,8 @@ if(true){
 // console.log(a)// error because a has scope inside if
 // console.log(b)// error because b has scope inside if
 console.log(c) //30 
-// Now it is a problem because c was initialised by some user as 300 and in some other file c=30.. this problem is created because var doesnt take care of scopes.. 
+// Now it is a problem because c was initialised by some user as 300 and in some other file c=30.. this problem is 
+// created because var doesnt take care of scopes.. 
 // Thats why let is used instead of var in JS
  // inside {} is is called block scope and outside it is global scope 
  // variable declared in global scope can be used can be used inside block scope but not vice versa
@@ -34,7 +36,8 @@ function one(){
         const website = "youtube"
         console.log(username) // hitesh// it can acccess of variables of outside level function one()
     }
-    // console.log(website) // error // only inner level have access of outerlevel variables , website is inner level variable in function two
+    // console.log(website) // error // only inner level have access of outerlevel variables ,
+    //  website is inner level variable in function two
     two()
 }
 one()
@@ -77,19 +80,27 @@ function addthree(num){
 const addfour=function(num){
     return num+4
 }
-// the point to be noted here is when you call function before initialization it worked in first method but will give error in second method 
+// the point to be noted here is when you call function before initialization it worked in first method but will 
+// give error in second method 
 // This is somehow related to hoisting which will be discussed later 
 
 
 // Hoisting:-
 /*
 🔹 What is Hoisting in JavaScript?
-Hoisting is a JavaScript mechanism where variables and function declarations are moved ("hoisted") to the top of their scope during the compilation phase, before the code is executed.
+
+Hoisting Definition according to MDN Docs:
+/* JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, 
+variables, classes, or imports to the top of their scope, prior to execution of the code. 
+
+Hoisting is a JavaScript mechanism where variables and function declarations are moved ("hoisted") 
+to the top of their scope during the compilation phase, before the code is executed.
 
 However, only the declaration is hoisted, not the initialization.
 
 🔹 Hoisting with var
-When you declare a variable using var, the declaration is hoisted to the top, but the initialization remains in place.
+When you declare a variable using var, the declaration is hoisted to the top, 
+but the initialization remains in place.
 
 Example 1: var is Hoisted
 var is hoisted but not in the Temporal Dead Zone (TDZ) like let and const. 
@@ -113,9 +124,14 @@ but they are not initialized and remain in a "Temporal Dead Zone" until their de
 
 Example 2: let and const Are Hoisted But Not Initialized (TDZ)
 
-console.log(y); // ReferenceError: Cannot access 'y' before initialization
+console.log(y); //Reference Error: Cannot access 'y' before initialization
 let y = 20;
 🔴 Unlike var, let does not get a default undefined value and throws an error.
+
+console.log(z) // Reference Error: z is not defined 
+NOTE: See error is different in case of y and z 
+i.eit is prove that y is hoisted but it is TDZ otherwise it would have given same error like z 
+i.e.Reference Error: z is not defined.
 
 🔹 Hoisting with Functions
 Function Declarations Are Fully Hoisted
@@ -127,18 +143,18 @@ function greet() {
 ✅ Function declarations are fully hoisted, meaning both their name and definition are moved to the top.
 
 Function Expressions Are NOT Hoisted
+*/
+// sayHi(); // TypeError: sayHi is not a function
 
-sayHi(); // TypeError: sayHi is not a function
-
-let sayHi = function() {
-  console.log("Hi!");
-};
-
-Your code will throw a ReferenceError, not a TypeError, 
+// let sayHi = function() {
+//   console.log("Hi!");
+// };
+/*
+Your code will throw a ReferenceError: Cannot access 'sayHi' before initialization,
 because let variables are in the Temporal Dead Zone (TDZ) until their declaration is executed.
 TDZ:-Temporal Dead Zone (TDZ)
 The variable exists but cannot be accessed before its declaration is executed.
-Accessing it before declaration throws a ReferenceError
+Accessing it before declaration throws a error ReferenceError: Cannot access 'sayHi' before initialization
 
 */
 
